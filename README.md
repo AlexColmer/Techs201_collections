f9f4257776add8f669f2510b5de970c14d005fce
Alex Colmer <alexander.colmer@gmail.com>
2023-01-31 09:31:40 +0000

    added images

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 ### lists are arrays in pretty much every other language 
 all about data management 
 
-to makea  list it needs to be `variable name = []`
+to make a  list it needs to be `variable name = []`
+use `print(type(variable name)` to work out what type you are using
+![](list.png)
+
+use `print(variable name[number of the item needed starts with 0]`
+
+Tuples same as lists, except they are immutable (you cant change any of the data from the list)
+
+to create a tuple you need to use `variable name = (List in here)`
+
+list slicing 
+
+- `print(mixture[1:3])` shows the list from the first to second number. 
+
+- `print(mixture[-2::])` double colon reverses the order.
+
+- `print(mixture[::2])`  bounces over the amount of indexes specified
--- lists_and_tuples.py
+++ lists_and_tuples.py
@@ -1,8 +1,8 @@
-# shopping_list = ["Milk", "eggs", "bread", "fruit", " cheese"]
+shopping_list = ["Milk", "eggs", "bread", "fruit", " cheese"]
 # print(type(shopping_list))
 # print(shopping_list)
 # print(shopping_list[0])
-# print(shopping_list[-1])
+print(shopping_list[-1])
 # # rewriting a value on the list
 # shopping_list[0] = "sugar"
 # print(shopping_list[0])
@@ -28,19 +28,19 @@
 # print(len(shopping_list))
 
 # mixed data type lists
-mixture = [1, 2, 3.5, "one", "two", "three"]
-print(mixture)
-#list slicing
-
-print(mixture[1:3])
-print(mixture[-2::]) # double colon reverses the order.
-print(mixture[::2]) # bounces over the amount of indexes specified
-
-#Tuples same as lists, except teh are immutable (you cant change any of the data from the list)
-# uses. they are useful for elements that you want ensuire stays the same.
-
+# mixture = [1, 2, 3.5, "one", "two", "three"]
+# print(mixture)
+# #list slicing
+#
+# print(mixture[1:3])
+# print(mixture[-2::]) # double colon reverses the order.
+# print(mixture[::2]) # bounces over the amount of indexes specified
+#
+# #Tuples same as lists, except teh are immutable (you cant change any of the data from the list)
+# # uses. they are useful for elements that you want ensuire stays the same.
+#
 essentials = ("bread", "eggs", "milk")
 print(essentials)
 print(type(essentials))
-
-# essentials[0] = "beans"
+#
+# # essentials[0] = "beans"
